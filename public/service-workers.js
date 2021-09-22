10fae46e6028c0d939a16c87e51cf169d1d07e6a
MehaletKB <mkesatebirhan@gmail.com>
2021-09-22 15:10:58 -0500

    fetch add

--- public/service-workers.js
+++ public/service-workers.js
@@ -14,6 +14,7 @@ const FILES_TO_CACHE = [
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
+
 self.addEventListener("install", (event) => {
     event.waitUntil(
         caches
@@ -24,7 +25,7 @@ self.addEventListener("install", (event) => {
     )
 });
 
-self.addEventListener("activate", function (event) {
+self.addEventListener("activate", (event) => {
     event.waitUntil(
         caches
         .keys()
@@ -40,4 +41,39 @@ self.addEventListener("activate", function (event) {
     })
 );
     self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+    if (event.request.url.includes("/api/")) {
+        event
+        .respondWith(
+            caches
+            .open(DATA_CACHE_NAME)
+            .then((cache) => {
+                return fetch(event.request)
+                .then((response) => {
+                    if (response.status === 200) {
+                        cache.put(event.request.url, response.clone());
+                    }
+                    return response;
+                })
+                .catch((error) => {
+                    return cache.match(event.request);
+                });
+            })
+            .catch((error) => console.log(error))
+        );
+        return;
+    }
+
+    event.respondWith(
+        caches
+        .open(CACHE_NAME)
+        .then((cache) => {
+            return cache.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            });
+        })
+    );
 });
